8990e41968a6dc8b6962b0476bfcec3e64605ca7
shadd0w <rene.gmitter@wame.sk>
2016-08-18 14:14:36 +0200

    UPDATE RouterModule

--- vendor/wame/AdminModule/grids/columns/RouteGridColumn.php
+++ vendor/wame/AdminModule/grids/columns/RouteGridColumn.php
@@ -8,10 +8,16 @@ class RouteGridColumn extends BaseGridColumn
 {
 	public function addColumn($grid) {
 		$grid->addColumnText('route', _('Route'))
+                ->setEditableCallback([$this, 'columnEdited'])
                 ->setSortable()
 				->setFilterText();
                 
 		return $grid;
 	}
+    
+    public function columnEdited($id, $value)
+    {
+        echo("Id: $id, new value: $value"); die;
+    }
     
 }
